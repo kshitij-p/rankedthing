1bfb536976c3313a814efe8ea34e9003b693fe16
kshitijp <pawarkshitij888@gmail.com>
2023-04-30 13:26:02 +0530

    remove example trpc router and prisma schema and install next themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,15 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neutral: {
+          1000: "#0d0d0d",
+        },
+        "dark-teal": "#0c0d0d",
+      },
+    },
   },
+  darkMode: "class",
   plugins: [],
 } satisfies Config;
