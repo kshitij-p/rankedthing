749a31e3b58fc9e5c8e41464ab36636b569ca0fa
kshitijp <pawarkshitij888@gmail.com>
2023-05-16 00:40:55 +0530

    add ci and make AuthButton show a loading message while auth status is loading

--- src/components/util/AuthButton.tsx
+++ src/components/util/AuthButton.tsx
@@ -1,23 +1,28 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import React, { type ForwardedRef } from "react";
+import { cn } from "~/lib/utils";
 import Button from "../ui/Button";
 
 const AuthButton = React.forwardRef(
   (
-    { variants, ...rest }: React.ComponentProps<typeof Button>,
+    { variants, className, ...rest }: React.ComponentProps<typeof Button>,
     passedRef: ForwardedRef<HTMLButtonElement>
   ) => {
-    const { data } = useSession();
+    const { data, status } = useSession();
+
+    const loading = status === "loading";
 
     return (
       <Button
         {...rest}
+        className={cn("relative flex items-center justify-center", className)}
         type="button"
         variants={{ type: "secondary", ...variants }}
         onClick={data ? () => void signOut() : () => void signIn("google")}
+        disabled={loading}
         ref={passedRef}
       >
-        {data ? "Sign out" : "Sign in"}
+        {loading ? "Loading" : data ? "Sign out" : "Sign in"}
       </Button>
     );
   }
